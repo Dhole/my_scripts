2911d58df6a7986f4d53efe65e6628cd8f3be8b5
Dhole <dhole@openmailbox.org>
2015-09-14 19:12:08 +0200

    Remove old stuff

--- puja_imatges_miageshack.sh
+++
@@ -1,2 +0,0 @@
-for files in *.gif *.jpg *.png; do curl -H Expect: -F fileupload="@$files" -F xml=yes -# "http://www.imageshack.us/index.php" | grep image_link | sed -e 's/<image_link>/[IMG]/g' -e 's/<\/image_link>/[\/IMG]/g'; done; 
-
