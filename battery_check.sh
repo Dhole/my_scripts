6c8e81860e95abed390594346b1db664592650a8
Dhole <dhole@openmailbox.org>
2016-01-03 19:54:17 +0100

    added script to check battery status

---
+++ battery_check.sh
@@ -0,0 +1,39 @@
+#! /bin/sh
+
+SLEEP_TIME=5   # Default time between checks.
+SAFE_PERCENT=30  # Still safe at this level.
+DANGER_PERCENT=20  # Warn when battery at this level.
+CRITICAL_PERCENT=10  # Suspend when battery at this level.
+
+export DISPLAY=:0.0
+
+notify() {
+        notify-send -u critical "Battery level is low: $1%\nSuspending soon..."
+}
+
+while [ true ]; do
+
+    state=$(acpi -b | grep -i discharging)
+    if [ "$state" != "" ]; then
+        rem_bat=$(acpi -b | grep -Eo "[0-9]+%" | grep -Eo "[0-9]+")
+
+        if [ $rem_bat -gt $SAFE_PERCENT ]; then
+            SLEEP_TIME=10
+        else
+            SLEEP_TIME=5
+            if [ $rem_bat -le $DANGER_PERCENT ]; then
+                SLEEP_TIME=2
+                notify $rem_bat
+            fi
+            if [ $rem_bat -le $CRITICAL_PERCENT ]; then
+                SLEEP_TIME=1
+                systemctl suspend &
+            fi
+        fi
+    else
+        SLEEP_TIME=10
+    fi
+
+    sleep ${SLEEP_TIME}m
+
+done
