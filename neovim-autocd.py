07c16d6007a44a21f9ed060327f4f698ce358e59
Dhole <dhole@riseup.net>
2018-05-21 00:53:08 -0700

    Update

---
+++ neovim-autocd.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import neovim
+import os
+
+nvim = neovim.attach('socket', path=os.environ['NVIM_LISTEN_ADDRESS'])
+nvim.vars['__autocd_cwd'] = os.getcwd()
+nvim.command('execute "lcd" fnameescape(g:__autocd_cwd)')
+del nvim.vars['__autocd_cwd']
